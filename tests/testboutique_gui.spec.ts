4c65b3ad42a2e3929464b004525175bc2b9953e4
TetianaZalata <sulga.tati@gmail.com>
2023-08-03 09:00:25 +0200

    Found the first bug - Spectometer broken link

--- tests/testboutique_gui.spec.ts
+++ tests/testboutique_gui.spec.ts
@@ -8,24 +8,24 @@ import { test, expect, chromium } from '@playwright/test';
     const context = await browser.newContext();
     const page = await context.newPage();
   
-
+  //Go to website
   await page.goto('https://testboutique-precisiontest.deno.dev/static/shop/');
   await page.click('text = here')
 
   //Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("PrecisionTestPro products and information");
 
-  //Expect TechSure Multimeter
+  //Expect TechSure Multimeter go to Wiki page
   await page.click('#products > div:nth-child(1) > div:nth-child(5) > a:nth-child(2)');
   await expect(page).toHaveURL("https://en.wikipedia.org/wiki/Multimeter");
+  await page.goBack();
+  await page.click('text = here')
+
+
+  //Expect NanoScan Spectrometer has broken link (1 bug)
+  await page.click('#products > div:nth-child(2) > div:nth-child(5) > a:nth-child(2)');
+  await expect(page).toHaveURL("https://en.wikipedia.org/wiki/Spectrometer");
+  
 });
 
-  //Expect 
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://testboutique-precisiontest.deno.dev/static/shop/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects the URL to contain intro.
-//   await expect(page).toHaveURL(/.*intro/);
+  
